9ab45428e26d9befd791af7cab029edbf27c710b
Purshotam <pankajpurshotam@gmail.com>
2023-08-03 15:54:42 +0530

    added simple api

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ const homeController = require('../controllers/home_controller');
 router.get('/' , homeController.home);
 router.use('/users' , require('./user'));
 
-router.use('/posts' , require('./posts'))
-router.use('/comments' , require('./comments'))
+router.use('/posts' , require('./posts'));
+router.use('/comments' , require('./comments'));
 
+//routes for postman apis
+router.use('/api' , require('./api'))
 module.exports = router;
