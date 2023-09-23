7c3ba8b51375953e45838c98dabd1ae53965a599
Purshotam <pankajpurshotam@gmail.com>
2023-09-23 16:05:48 +0530

    for deploy

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose');
 
 
-// const url = 'mongodb+srv://pankajpurshotam:patel123@cluster0.l9qysyp.mongodb.net/?retryWrites=true&w=majority';
+const url = 'mongodb+srv://pankajpurshotam:patel123@cluster0.l9qysyp.mongodb.net/?retryWrites=true&w=majority';
 
-// mongoose.connect(url ,{
-//   useNewUrlParser: true,
-// useUnifiedTopology: true,
-// });
-// const db=mongoose.connection;
+mongoose.connect(url ,{
+  useNewUrlParser: true,
+useUnifiedTopology: true,
+});
+const db=mongoose.connection;
 
 main().catch(err => console.log(err));
 
 //connecting mangoose
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/codify_development');
+// async function main() {
+//   await mongoose.connect('mongodb://127.0.0.1:27017/codify_development');
 
-}
+// }
 
-//connecting db to mangoose connection 
-const db = mongoose.connection;
+// //connecting db to mangoose connection 
+// const db = mongoose.connection;
 
 
 
